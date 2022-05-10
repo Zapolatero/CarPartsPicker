c6760c9eb88a3a6c72a84b9b896d0e596479d121
Louis Pascuzzi <pascuzzilouis@gmail.com>
2022-05-10 15:21:44 +0200

    project initiation

---
+++ carPartsPicker/src/main/java/com/example/carPartsPicker/CarPartsPickerApplication.java
@@ -0,0 +1,13 @@
+package com.example.carPartsPicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPartsPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPartsPickerApplication.class, args);
+	}
+
+}
---
+++ carPartsPicker/src/test/java/com/example/carPartsPicker/CarPartsPickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carPartsPicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPartsPickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
