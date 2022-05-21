d8261c09744d431595c4de04b51f258d958a1c67
Zapolatero <pascuzzilouis@gmail.com>
2022-05-21 12:29:38 +0200

    Addition of pages displaying the various categories, each car part in a category, and the data of a selected carPart

--- carPartsPicker/carPartsPicker/src/main/java/com/MO54/carPartsPicker/controller/MainController.java
+++ carPartsPicker/carPartsPicker/src/main/java/com/MO54/carPartsPicker/controller/MainController.java
@@ -1,24 +1,17 @@
 package com.MO54.carPartsPicker.controller;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import com.MO54.carPartsPicker.model.CarPart;
-import com.MO54.carPartsPicker.model.MainCategory;
-import com.MO54.carPartsPicker.model.SubCategory;
-import com.MO54.carPartsPicker.repository.SubCategoryRepository;
+import com.MO54.carPartsPicker.model.Category;
 import com.MO54.carPartsPicker.services.CarPartService;
+import com.MO54.carPartsPicker.services.CategoryServiceImplementation;
 import com.MO54.carPartsPicker.services.MainCategoryServiceImplementation;
-import com.MO54.carPartsPicker.services.MainCategoryService;
 import com.MO54.carPartsPicker.services.SubCategoryServiceImplementation;
-import com.MO54.carPartsPicker.services.SubCategoryService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -28,6 +21,8 @@ public class MainController {
     @Autowired
     private CarPartService carPartService;
     @Autowired
+    private CategoryServiceImplementation categoryService;
+    @Autowired
     private MainCategoryServiceImplementation mainCategoryService;
     @Autowired
     private SubCategoryServiceImplementation subCategoryService;
@@ -36,6 +31,23 @@ public class MainController {
     public ModelAndView List() {
         ModelAndView model = new ModelAndView("index");
         model.addObject("mainCategoriesList", mainCategoryService.getAllMainCategories());
+        return model;
+    }
+
+    @RequestMapping(value = "/carPartsList", method = RequestMethod.GET)
+    public ModelAndView listCarPartsByCategory(@RequestParam(required = true) Integer categoryId) {
+        ModelAndView model = new ModelAndView("productsList");
+        Category requestedCategory = categoryService.getCategoryById(categoryId);
+        model.addObject("categoryName", requestedCategory.getName());
+        model.addObject("carPartsList", requestedCategory.getCarParts());
+        return model;
+    }
+
+    @RequestMapping(value = "/carPart", method = RequestMethod.GET)
+    public ModelAndView displayCarPartById(@RequestParam(required = true) Integer carPartId) {
+        ModelAndView model = new ModelAndView("carPart");
+        CarPart requestedCarPart = carPartService.getCarPartById(carPartId);
+        model.addObject("carPart", requestedCarPart);
         return model;
     }
 
