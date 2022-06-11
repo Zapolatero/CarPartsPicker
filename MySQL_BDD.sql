d99b66c83920df49ec727411b458c25d3e7a270e
Zapolatero <pascuzzilouis@gmail.com>
2022-06-11 22:02:19 +0200

    Updated My_SQL_BDD.sql : it now creates carpartspicker data base if it doesn't exist

--- MySQL_BDD.sql
+++ MySQL_BDD.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `carpart`
 --
 
+CREATE DATABASE IF NOT EXISTS carpartspicker;
+USE carpartspicker;
+
 DROP TABLE IF EXISTS `carpart`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
